143c1d50fe315b99d8b539b7898588dd7e9b4586
Toru YABUKI <yabuuuuu@gmail.com>
2020-09-12 18:47:11 +0900

    Should be set to false in production

--- php/app/settings.php
+++ php/app/settings.php
@@ -8,7 +8,7 @@ return function (ContainerBuilder $containerBuilder) {
     // Global Settings Object
     $containerBuilder->addDefinitions([
         'settings' => [
-            'displayErrorDetails' => true, // Should be set to false in production
+            'displayErrorDetails' => false, // Should be set to false in production
             'logger' => [
                 'name' => 'slim-app',
                 'path' => 'php://stdout', // __DIR__ . '/var/log/app.log'
